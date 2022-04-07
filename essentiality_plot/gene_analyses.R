79d403acbc7ec2e598cd6e7c6c2146c511a334eb
Wang Long <wanglong@nju.edu.cn>
2022-04-07 22:59:09 +0800

    Update essentiality data

--- essentiality_plot/gene_analyses.R
+++ essentiality_plot/gene_analyses.R
@@ -7,14 +7,14 @@ gene_data<-fread("data/gene_level_data.mut_counts.csv")
 
 # we can examine for example, mutation rates in essential vs non-essential functional categories of genes
 #mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum((SNV+InDel)*`CDS length`), length=sum(`CDS length`)), by=.(essentiality)]
+#mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Monroe CDS SNM Count`), length=sum(`Local CDS length`)), by=.(essentiality)]
+#mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Monroe intron SNM Count`), length=sum(`Local Intron length`)), by=.(essentiality)]
+#mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng CDS SNM Count`), length=sum(`Local CDS length`)), by=.(essentiality)]
+#mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng intron SNM Count`), length=sum(`Local Intron length`)), by=.(essentiality)]
 #mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Monroe CDS Count`), length=sum(`Local CDS length`)), by=.(essentiality)]
 #mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Monroe intron Count`), length=sum(`Local Intron length`)), by=.(essentiality)]
 #mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng CDS Count`), length=sum(`Local CDS length`)), by=.(essentiality)]
-#mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng intron Count`), length=sum(`Local Intron length`)), by=.(essentiality)]
-#mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Monroe INDEL Count`), length=sum(`Overall length`)), by=.(essentiality)]
-mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng INDEL Count`), length=sum(`Overall length`)), by=.(essentiality)]
-
-
+mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng intron Count`), length=sum(`Local Intron length`)), by=.(essentiality)]
 
 ggplot(mutation_essentiality, aes(x=essentiality, y=mutations/length))+
   geom_bar(stat="identity")
