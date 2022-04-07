44f0ea05b70ba1c29db53dcc91eef256020b7051
Wang Long <noitulove9891@gmail.com>
2022-04-07 19:52:10 +0800

    Add files via upload

--- essentiality_plot/gene_analyses.R
+++ essentiality_plot/gene_analyses.R
@@ -3,16 +3,19 @@ library(ggplot2)
 
 # gene level data contains epigenomic, functional, and mutation data for coding regions of each gene
 #gene_data<-fread("data/gene_level_data.csv")
-gene_data<-fread("data/gene_level_data.SNMs.csv")
+gene_data<-fread("data/gene_level_data.mut_counts.csv")
 
 # we can examine for example, mutation rates in essential vs non-essential functional categories of genes
 #mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum((SNV+InDel)*`CDS length`), length=sum(`CDS length`)), by=.(essentiality)]
 #mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Monroe CDS Count`), length=sum(`Local CDS length`)), by=.(essentiality)]
 #mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Monroe intron Count`), length=sum(`Local Intron length`)), by=.(essentiality)]
 #mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng CDS Count`), length=sum(`Local CDS length`)), by=.(essentiality)]
-mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng intron Count`), length=sum(`Local Intron length`)), by=.(essentiality)]
+#mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng intron Count`), length=sum(`Local Intron length`)), by=.(essentiality)]
+#mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Monroe INDEL Count`), length=sum(`Overall length`)), by=.(essentiality)]
+mutation_essentiality<-gene_data[essentiality!="" ,.(mutations=sum(`Weng INDEL Count`), length=sum(`Overall length`)), by=.(essentiality)]
 
 
 
 ggplot(mutation_essentiality, aes(x=essentiality, y=mutations/length))+
   geom_bar(stat="identity")
+  
